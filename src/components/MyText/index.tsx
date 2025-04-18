cf244840647d95200b5f2d66743460dc7e0895c0
Felipe Leal <felipewanderson2020@gmail.com>
2025-04-18 14:23:26 -0300

    feat: handle onPress

--- src/components/MyText/index.tsx
+++ src/components/MyText/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Alert, Text } from 'react-native';
 import {style} from './styles';
 
 type MyTextProps = {
   yournameherefromprops: string
 }
 
+function handleOnPress(){
+  Alert.alert('You just pressed the text component');
+}
+
 const MyText: React.FC<MyTextProps> = ({ yournameherefromprops = ''}) => {
-  return <Text style={style.myText}>{`Hello, ${yournameherefromprops}`}</Text>;
+  return <Text style={style.myText} onPress={handleOnPress}>{`Hello, ${yournameherefromprops}`}</Text>;
 };
 
 export default MyText;
